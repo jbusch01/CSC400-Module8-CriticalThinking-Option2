bdff3540d69896c089066c92f9a70a62879c0d00
jbusch01 <jonrbusch@gmail.com>
2025-08-02 13:00:39 -0600

    Committing before making updates to course name input

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        InstructorQueue instructorQueue = new InstructorQueue();
+
+        System.out.println("Enter information for 5 instructors:\n");
+        
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Instrucotr " + i + ":");
+            
+            System.out.println("First Name: ");
+            String firstName = scnr.nextLine();
+            
+            System.out.println("Last Name: ");
+            String lastName = scnr.nextLine();
+
+            System.out.println("Enter course names (serperated by a comma): ");
+            String courseInput = scnr.nextLine();
+            String[] courses = courseInput.split(",");
+
+            // Trim whitespace from each course name
+            for (int j = 0; j < courses.length; j++) {
+                courses[j] = courses[j].trim();
+            }
+
+            String courseName = courses[0];
+            int numCourses = courses.length;
+        }
+    }
+    
+}
